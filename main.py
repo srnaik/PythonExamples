abfa09df453f090cb43a1fee919ee2e376a1193a
srnaik <srnaik@hotmail.com>
2026-04-15 23:04:40 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import platform
+
+
+print('Hello World')
+print('This is my python version {}'.format(platform.python_version()))
