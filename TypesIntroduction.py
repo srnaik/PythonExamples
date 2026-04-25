f9f4690348da23b15a2b3ab441ebe1f3e79079c9
srnaik <srnaik@hotmail.com>
2026-04-24 22:30:36 -0700

    Adding a method with string type

--- TypesIntroduction.py
+++ TypesIntroduction.py
@@ -2,5 +2,14 @@ def print_num(num):
     print('x is {}'.format(num))
     print(type(num))
 
+def print_str():
+    name = 'sachin'
+    print('Name is {}'.format(name))
+    name = 'sachin'.capitalize()
+    print('Name with Capital Letter {}'.format(name))
+    name = 'sachin'.upper()
+    print('Name with Capital Letter {}'.format(name))
+
 if __name__ == '__main__':
     print_num(7)
+    print_str()
