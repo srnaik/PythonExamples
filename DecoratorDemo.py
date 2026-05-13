648a885ce1055c824991805eaf7e2b66cf44e5a4
srnaik <srnaik@hotmail.com>
2026-05-12 22:20:39 -0700

    Decorator Example

--- DecoratorDemo.py
+++ DecoratorDemo.py
@@ -1,1 +1,18 @@
 # Decorator is form a metaprogramming
+
+
+def func(f):
+    #print('This is func')
+    def func2():
+        print(f'This is before the function call {f}')
+        f()
+        print(f'This is after the function call {f}')
+    return func2
+
+@func
+def second_func():
+    print('This is the second function')
+
+# x = func(second_func)
+# x()
+second_func()
