4d99a644d9a4dd107e6365a0d048b31e17772ca8
srnaik <srnaik@hotmail.com>
2026-05-20 22:28:04 -0700

    Updating constructor with default values, adding setter/getter and overriding __str__ method

--- Animal.py
+++ Animal.py
@@ -2,19 +2,26 @@
 class Animal:
 
     def __init__(self, **kwargs):
-        self._type = kwargs['type']
-        self._name = kwargs['name']
-        self._sound = kwargs['sound']
+        self._type = kwargs['type'] if 'type' in kwargs else 'Kitten'
+        self._name = kwargs['name'] if 'name' in kwargs else 'Duck'
+        self._sound = kwargs['sound'] if 'sound' in kwargs else 'rwar'
 
 
-    def type(self):
-        return self._type
+    def type(self, t = None):
+       if t: self._type = t
+       return self._type
 
-    def name(self):
+    def name(self, n = None):
+        if n: self._name = n
         return self._name
 
-    def sound(self):
+    def sound(self, s = None):
+        if s: self._sound = s
         return self._sound
+
+    # Overriding __str__ method from object class
+    def __str__(self):
+        return f'The {self.type()} is named "{self.name()}" and says "{self.sound()}".'
 
 def print_animal(obj):
         if not isinstance(obj,Animal):
@@ -27,5 +34,6 @@ def main():
     print_animal(first_animal)
     print_animal(second_animal)
     print_animal(Animal(type='velociraptor',name='veronica',sound='hello'))
+    print(first_animal) # overridden __str__ method is invoked here
 
 if __name__ == '__main__': main()
